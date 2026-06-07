b8d4bcf4a99331aefd2e6fa423574a0122cece3d
Oswaldo Barros <88897625+osbarros@users.noreply.github.com>
2026-06-07 19:40:26 -0300

    Add: baseball-game - submission-15

---
+++ Data Structures & Algorithms/baseball-game/submission-15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        result = 0
+        notDigits = {"D", "C", "+"}
+        for i in range(len(operations)):
+            op = operations[i]
+            if op not in notDigits:
+                record.append(int(op))
+                result += int(op)
+            if op == '+':
+                previous2 = record[-1] + record[-2]
+                record.append(previous2)
+                result += previous2
+            if op == 'C':
+                if len(record) > 0:
+                    inv = record.pop()
+                    result -= inv
+            if op == 'D':
+                record.append(record[-1] * 2)
+                result += record[-1]
+            print(f"Op: {op}, Result: {result}, record: {record}")
+        return result
