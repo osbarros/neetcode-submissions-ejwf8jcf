3023aa3f8ae03155add39ed4e48f0c18dfb8a3e0
Oswaldo Barros <88897625+osbarros@users.noreply.github.com>
2026-06-10 19:42:50 -0300

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,69 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        L = 0
+        R = len(s) - 1
+        count
+        isPalindrome = True
+        secondChanceR = 0
+        secondChanceL = 0
+
+        while L < R:
+            if s[R] != s[L]:
+                secondChanceR = R
+                secondChanceL = L
+                isPalindrome = False
+                break
+            
+            L += 1
+            R -= 1
+        
+        if isPalindrome == True:
+            return True
+        
+        isPalindrome = True
+
+        print(f"second chance L: {secondChanceL}")
+        print(f"second chance R: {secondChanceR}")
+        
+        L = 0
+        R = len(s) - 1
+        while L < R:
+
+            print(s[R] , s[L])
+            if L == secondChanceL:
+                L += 1
+                continue
+            if s[R] != s[L]:
+                isPalindrome = False
+                break 
+            L += 1
+            R -= 1
+
+        if isPalindrome == True:
+            return True 
+
+        isPalindrome = True   
+        L = 0
+        R = len(s) - 1 
+
+        while L < R:
+            if R == secondChanceR:
+                R -= 1
+                continue
+
+            if s[R] != s[L]:
+                isPalindrome = False
+                break 
+            L += 1
+            R -= 1
+            
+        return isPalindrome
+                
+
+
+
+
+
+
+
+
