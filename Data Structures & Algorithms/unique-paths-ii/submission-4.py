9385adc600b4451f3595bbfa1baf3f7cc90d7891
Oswaldo Barros <88897625+osbarros@users.noreply.github.com>
2026-09-04 18:08:44 -0300

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bruteForce(self, r, c, rows, columns, obstacleGrid, cache):
+        if r == rows or c == columns or obstacleGrid[r][c] == 1:
+            return 0
+        
+        if r == rows - 1 and c == columns - 1:
+            return 1
+        
+        if cache[r][c] != -1:
+            return cache[r][c]
+
+        cache[r][c] = self.bruteForce(r + 1, c, rows, columns, obstacleGrid, cache) + self.bruteForce(r, c + 1, rows, columns, obstacleGrid, cache)
+
+        return cache[r][c]
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        cache = [[-1 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+        return self.bruteForce(0,0,len(obstacleGrid), len(obstacleGrid[0]), obstacleGrid, cache)
+
+        
