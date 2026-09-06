e609265e2d6b44659f83a9c6e904735e28639336
Oswaldo Barros <88897625+osbarros@users.noreply.github.com>
2026-09-06 14:31:44 -0300

    Add: course-schedule-iv - submission-6

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-6.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def dfs(self, courses, course, courseA, visited):
+
+        if course in visited or course in self.reachable[courseA]:
+            return
+        visited.add(course)
+
+        if course != courseA:
+            self.reachable[courseA].add(course)
+        
+        
+        for c in range(len(courses[course])):
+            self.dfs(courses,courses[course][c], courseA, visited)
+        
+
+
+
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+
+        courses = defaultdict(list)
+        for prereq in prerequisites:
+            courses[prereq[0]].append(prereq[1])
+        answer = []
+        self.reachable = defaultdict(set)
+        
+
+        for i in range(numCourses):
+            visited = set()
+            self.dfs(courses, i, i, visited)
+
+        for querie in queries:
+            if querie[1] in self.reachable[querie[0]]:
+                answer.append(True)
+            else:
+                answer.append(False)
+
+        return answer
+
+        
